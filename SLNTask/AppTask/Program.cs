1c8bb70742727bdf6550b116acf572c51ee46e2f
REGINALDO REIS DE SANTANA <113715701+reginaldosenai@users.noreply.github.com>
2026-07-31 10:27:01 -0300

    [BR] readme e projeto incial pronto

--- SLNTask/AppTask/Program.cs
+++ SLNTask/AppTask/Program.cs
@@ -1,7 +1,13 @@
+using AppTask.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbTasksContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSqlServer")));
 
 var app = builder.Build();
 
