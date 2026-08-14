2ce739bd7f99107666337a2daa5cdfde4a042e85
Daniel Braga Gomes <danielbragagomes2022@gmail.com>
2026-08-14 09:38:46 -0300

    Incrementação Departamento CRUD

---
+++ SLNTask/AppTask/Models/Departamento.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppTask.Models
+{
+    public class Departamento
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        public string Descricao { get; set; } = null!;
+
+        public Boolean Ativo { get; set; }
+    }
+}
