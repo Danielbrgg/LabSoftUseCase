74ebe3be22be42a95263e501d8564ed731708ba0
Daniel Braga Gomes <danielbragagomes2022@gmail.com>
2026-08-28 11:20:00 -0300

    Finalzinho

---
+++ AspTaskZero/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspTaskZero.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AspTaskZero/Program.cs
@@ -0,0 +1,35 @@
+using AspTaskZero.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+// Registrando o DbContext com a String de Conexão
+builder.Services.AddDbContext<DbTasksZeroContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSqlServer")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
