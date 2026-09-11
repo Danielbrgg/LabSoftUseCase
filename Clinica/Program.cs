2cf0dfe3245e56f6ff620b9c991c410e9b41a957
Daniel Braga Gomes <danielbragagomes2022@gmail.com>
2026-09-11 15:09:00 -0300

    feat(clinica): melhorar acesso e experiencia do usuario

--- Clinica/Program.cs
+++ Clinica/Program.cs
@@ -4,17 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar os serviços ao contêiner
 builder.Services.AddControllersWithViews();
 
-// Registrar o DbContext com a string de conexão
 builder.Services.AddDbContext<DbClinicaContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("ConexaoSqlServer")
     )
 );
 
-// Configuração da Session
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -22,36 +19,32 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-// Configuração da autenticação por Cookie
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = "/Account/Login";
-        options.AccessDeniedPath = "/Account/Login";
+        options.AccessDeniedPath = "/Consulta/Index";
         options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.SlidingExpiration = true;
     });
 
 var app = builder.Build();
 
-// Configuração do tratamento de erros
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
-// Middleware
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseSession();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Configuração das rotas
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}"
